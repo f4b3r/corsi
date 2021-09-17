c970bd92891b33bb868bfe8b40763a983ffc88af
fabrizio <pititto.fabrizio@gmail.com>
2021-09-17 12:56:51 +0200

    add pagination

--- gestione utenti/js/functions.js
+++ gestione utenti/js/functions.js
@@ -117,31 +117,7 @@ $(function () {
   
 
 
-  $("#numRisultati").on('change' , function(){
-
-    $.ajax({
-      url: 'https://randomuser.me/api/?results=' + $('#'),
-      dataType: 'json',
-      success: function(data) {
-        //let users = JSON.parse(data)
-        console.log(data);
-        let utenti = data.results;
-        $each(utenti, function(index, utente)
-        {
-          let tr = $("<tr></tr>");
-          let td1 = $("<td></td>").text(utente.id.name);
-          let td2 = $("<td></td>").text(utente.id.name);
-        }
-        
-        );
-       
-      },
-      error: function(data){
-        console.error(data);
-      }
-    });
-
-  })
+  
 var sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
 sleep(1000).then(
@@ -156,6 +132,36 @@ poll(() => new Promise(() => console.log('Hello World!')), 1000)
         
 
  */
+
+$("#numRisultati").on('change' , function(){
+  $.ajax({
+    url: 'https://randomuser.me/api/?results=' + $('#numRisultati').val() + '&seed=fabriziopititto',
+    dataType: 'json',
+    success: function(data) {
+      //let users = JSON.parse(data)
+      console.log(data);
+      $('#bodyTable').empty();
+      let utenti = data.results;
+      let currentPage = data.info.page;
+      $('#currentPage').val(currentPage);
+      $.each(utenti, function(index, utente)
+      {
+       renderTable(utente);
+
+
+      }
+      
+      );
+     
+    },
+    error: function(data){
+      console.error(data);
+    }
+  });
+
+})
+
+
   function renderTable(utente) {
     let tBody = document.getElementById('bodyTable');
     let tr = document.createElement('tr');
@@ -165,6 +171,9 @@ poll(() => new Promise(() => console.log('Hello World!')), 1000)
     let td2 = document.createElement('td');
     let td3 = document.createElement('td');
     let td4 = document.createElement('td');
+    let panel = document.createElement('div');
+    panel.id="panel";
+   
     img.src = utente.picture.medium;
 
     td1.innerHTML = utente.name.first + ' ' + utente.name.last;
@@ -173,13 +182,70 @@ poll(() => new Promise(() => console.log('Hello World!')), 1000)
     td4.innerHTML = "<td><i class='far fa-trash-alt'></i></td>";
     tdImg.appendChild(img);
     tr.append(tdImg, td1, td2, td3, td4);
-    tBody.appendChild(tr);
+    tBody.append(tr,panel);
+    
+    
   }
 
+  $(document).on('click', 'tr',function(){
+    console.log($(this).siblings().first('#panel'));
+    $($(this).siblings().first('#panel')).slideToggle("slow");
+  });
+  function renderPagination(){
+   
+    $('.pagination').append($("<li class='page-item' id='prevLI'></li>"));
+   
+    $(document).find('#prevLI').append($("<a  class='page-link' href=#></a>").text('Previous'));
+   
+    
+   
+    for( i = 1; i <= 10; i++){
+     
+      $('.pagination').append($("<li class='page-item'><a  class='page-link' href=#>"+ 
+       i +"</a></li>"));
+      
+    }
+    $('.pagination').append($("<li class='page-item' id='nextLI'></li>"));
+    $(document).find('#nextLI').append($("<a class='page-link' href=#></a>").text('Next'));
+   
+  }
+ 
+  renderPagination();
+
+  $(document).on('click' , '.page-link', function(e){
+    let numPage;
+   e.preventDefault();
+    if($(this).text() === 'Next'){
+      numPage = parseInt($('#currentPage').val()) + 1
+    }else if  ($(this).text() === 'Previous'){
+      numPage =  parseInt($('#currentPage').val()) - 1 > 0 ?  parseInt($('#currentPage').val()) - 1 : 1;
+    }else{
+      numPage = $(this).text() ;
+    }
+   fetch('https://randomuser.me/api/?page=' + numPage + '&results= ' + $('#numRisultati').val() + '&seed=fabriziopititto',).then(
+
+    function (data) {
+      return data.json();
+    }
 
 
+  ).then(
+    (result) => {
+      $('#bodyTable').empty();
+      let utenti = result.results;
+     
+      let currentPage = result.info.page;
+      $('#currentPage').val(currentPage);
+      return utenti.map(function (utente) {
+        renderTable(utente);
+      });
+    }
+  )
+
+  })
   $('#search').on('click', function (e) {
     e.preventDefault();
+    if($('#inputSearch').val().trim() !== ''){
     let rows = $('#bodyTable').children('tr');
    
     var rowArray = Array.prototype.slice.call(rows);
@@ -191,11 +257,13 @@ poll(() => new Promise(() => console.log('Hello World!')), 1000)
  
     $('#bodyTable').empty();
     filtered.forEach((input) => $('#bodyTable').append(input));
-
+    }
   });
 
 
-  fetch('https://randomuser.me/api/?results=10').then(
+
+
+  fetch('https://randomuser.me/api/?results=10&seed=fabriziopititto').then(
 
     function (data) {
       return data.json();
@@ -206,13 +274,12 @@ poll(() => new Promise(() => console.log('Hello World!')), 1000)
     (result) => {
       let utenti = result.results;
       console.log(utenti);
+      let currentPage = result.info.page;
+      $('#currentPage').val(currentPage);
+
       return utenti.map(function (utente) {
         renderTable(utente);
       });
-
-
-
-
     }
   )
 });
